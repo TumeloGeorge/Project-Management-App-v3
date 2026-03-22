77ccde1af603e563e16d6d60aa39a935489c9592
TumeloGeorge <tumeloge@gmail.com>
2026-03-22 15:20:59 +0200

    modified:   src/environments/environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   firebase: {
-  apiKey: "AIzaSyC5RoVyt_CQqZyI4q3LszWXkoHO4zj0l9o",
+  apiKey: process.env.apikey,
   authDomain: "project-manager-27da3.firebaseapp.com",
   projectId: "project-manager-27da3",
   storageBucket: "project-manager-27da3.firebasestorage.app",
